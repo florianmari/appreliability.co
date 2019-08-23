5b9a902fd821914af16d168679007cf9f2f657b8
Florian Mari <fmari@deezer.com>
2019-08-23 17:46:46 +0200

    Shorter, better intro

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,5 +2,5 @@ import React from "react"
 import ExternalLink from "../components/externalLink"
 
 export default () => (
-    <p>This website is about what to care in order to build reliable mobile applications. <ExternalLink name="Feel free to contribute" link="https://github.com/florianmari/appreliability.co" />. This is inspired from several ressources for offering you a checklist to build resilient cliend-side applications. <br /> <ExternalLink name="My twitter" link="https://twitter.com/florianmari"/></p>
+    <p>This is inspired from several ressources for offering you a checklist to build resilient cliend-side applications. <ExternalLink name="Feel free to contribute" link="https://github.com/florianmari/appreliability.co" /> <br /><br /> <ExternalLink name="My twitter" link="https://twitter.com/florianmari"/></p>
 )
