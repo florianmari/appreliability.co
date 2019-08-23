dfe5c75d509444d94bbbd3ad9bac264949f047c1
Florian Mari <fmari@deezer.com>
2019-08-23 17:08:04 +0200

    External linker component

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,5 +1,6 @@
 import React from "react"
+import ExternalLink from "../components/externalLink"
 
 export default () => (
-    <p>This is a simple checklist about what to care in mobile reliability engineering. Feel free to contribute.</p>
+    <p>This is a simple checklist about what to care in mobile reliability engineering. <ExternalLink name="Feel free to contribute" link="https://github.com/florianmari/appreliability.co" />.</p>
 )
