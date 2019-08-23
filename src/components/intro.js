0b603f8e69b292e8ffa7214a860c41db50924fac
Florian Mari <fmari@deezer.com>
2019-08-23 17:50:57 +0200

    Separation

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,5 +2,5 @@ import React from "react"
 import ExternalLink from "../components/externalLink"
 
 export default () => (
-    <p>This is inspired from several ressources for offering you a checklist to build resilient cliend-side applications. <ExternalLink name="Feel free to contribute" link="https://github.com/florianmari/appreliability.co" /> <br /><br /> <ExternalLink name="My twitter" link="https://twitter.com/florianmari"/> <ExternalLink name="My github" link="https://github.com/florianmari"/></p>
+    <p>This is inspired from several ressources for offering you a checklist to build resilient cliend-side applications. <ExternalLink name="Feel free to contribute" link="https://github.com/florianmari/appreliability.co" /> <br /><br /> <ExternalLink name="My twitter" link="https://twitter.com/florianmari"/> - <ExternalLink name="My github" link="https://github.com/florianmari"/></p>
 )
